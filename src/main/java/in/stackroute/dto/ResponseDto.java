80adfa19094f4a1081645cfd0769762384737099
ashish-panicker <ashish.s.panicker@proton.me>
2025-04-03 09:51:50 +0530

    BugFixed: Receiving 406 after a successful insert.

--- src/main/java/in/stackroute/dto/ResponseDto.java
+++ src/main/java/in/stackroute/dto/ResponseDto.java
@@ -1,9 +1,13 @@
 package in.stackroute.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.http.HttpStatus;
 
 @Builder
+@Getter @Setter @AllArgsConstructor
 public class ResponseDto {
     private HttpStatus status;
     private Object message;
