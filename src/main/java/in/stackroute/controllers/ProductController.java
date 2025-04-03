e4833bb57717321de7a1bca2aaf07856a39be834
ashish-panicker <ashish.s.panicker@proton.me>
2025-04-03 12:03:21 +0530

    Validations, Validation Exception Handling.

--- src/main/java/in/stackroute/controllers/ProductController.java
+++ src/main/java/in/stackroute/controllers/ProductController.java
@@ -4,12 +4,14 @@ import in.stackroute.domain.Product;
 import in.stackroute.dto.ProductDto;
 import in.stackroute.dto.ResponseDto;
 import in.stackroute.service.ProductService;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/products")
@@ -32,18 +34,41 @@ public class ProductController {
      * }
      */
     @PostMapping
-    public ResponseEntity<ResponseDto> createProduct(@RequestBody ProductDto dto) {
+    public ResponseEntity<ResponseDto> createProduct(@Valid @RequestBody ProductDto dto) {
 //        Product product = new Product(dto.getProductName(), dto.getPrice(),  dto.getStock());
+//        To be used if BindingResult parameter is supplied to the controller methods
+//        if(result.hasErrors()){
+//            Map<Object, Object> errors = new HashMap<>();
+//            result.getFieldErrors()
+//                    .forEach(fe -> errors.put(fe.getField(), fe.getDefaultMessage()));
+//            var response = ResponseDto.builder()
+//                    .message(errors)
+//                    .status(HttpStatus.BAD_REQUEST)
+//                    .build();
+//            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+//        }
         var product = Product.builder()
                 .productName(dto.getProductName())
                 .price(dto.getPrice())
                 .stock(dto.getStock())
                 .build();
-        var result = productService.save(product);
+        var res = productService.save(product);
         var response = ResponseDto.builder()
-                .message("Product created with id: " +result.getProductId())
+                .message("Product created with id: " + res.getProductId())
                 .status(HttpStatus.CREATED)
                 .build();
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ResponseDto> handleValidationErrors(MethodArgumentNotValidException e){
+        Map<Object, Object> errors = new HashMap<>();
+        e.getFieldErrors()
+                .forEach(fe -> errors.put(fe.getField(), fe.getDefaultMessage()));
+        var response = ResponseDto.builder()
+                .message(errors)
+                .status(HttpStatus.BAD_REQUEST)
+                .build();
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
     }
 }
